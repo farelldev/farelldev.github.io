c63bf57623f6d0f6e82a3045b959c270b4a8e502
Farelldeo Lionel Chalvary <farelldeolionelchalvary@gmail.com>
2026-07-27 22:53:52 +0700

    style: add smooth animation for sections, Close #2

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,15 @@ button.addEventListener("click", () => {
     menu.classList.toggle("hidden");
     menu.classList.toggle("flex");
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".hidden-section").forEach((el) => {
+    observer.observe(el);
+});
